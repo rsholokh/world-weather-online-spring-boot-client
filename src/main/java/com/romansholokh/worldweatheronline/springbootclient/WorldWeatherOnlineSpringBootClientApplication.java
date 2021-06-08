495fd74421ecbe7abb6a4a97dfd4be0ec6a679e3
Roman <sholokh.r@gmail.com>
2021-06-08 13:18:36 +0200

    Initial commit

---
+++ src/main/java/com/romansholokh/worldweatheronline/springbootclient/WorldWeatherOnlineSpringBootClientApplication.java
@@ -0,0 +1,13 @@
+package com.romansholokh.worldweatheronline.springbootclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldWeatherOnlineSpringBootClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorldWeatherOnlineSpringBootClientApplication.class, args);
+    }
+
+}
