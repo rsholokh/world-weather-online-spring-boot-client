dee3dc85f3737c9297766aa06970d2d9ff6a6d59
Roman <sholokh.r@gmail.com>
2021-06-08 18:16:50 +0200

    Added connection details to db in application.properties. Created schema.sql with weather table

---
+++ src/main/resources/sсhema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS weather;
+
+CREATE TABLE weather (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    city VARCHAR(50) NOT NULL,
+    date DATE NOT NULL,
+    max_temp VARCHAR(3) NOT NULL,
+    avg_temp VARCHAR(3) NOT NULL,
+    min_temp VARCHAR(3) NOT NULL,
+    number_of_inquiries INT NOT NULL,
+);
