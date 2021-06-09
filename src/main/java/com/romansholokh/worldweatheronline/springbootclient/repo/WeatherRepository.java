fccc47991c904539a175082712a14d8de3028c6a
Roman <sholokh.r@gmail.com>
2021-06-09 22:20:05 +0200

    Create empty WeatherController, WeatherService & WeatherRepository

---
+++ src/main/java/com/romansholokh/worldweatheronline/springbootclient/repo/WeatherRepository.java
@@ -0,0 +1,11 @@
+package com.romansholokh.worldweatheronline.springbootclient.repo;
+
+import com.romansholokh.worldweatheronline.springbootclient.entity.Weather;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WeatherRepository extends JpaRepository<Weather, Integer> {
+
+
+}
