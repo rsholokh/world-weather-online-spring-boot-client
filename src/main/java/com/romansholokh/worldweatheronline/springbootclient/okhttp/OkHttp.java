31c7474bd0115f608566b5b4f78ac7e2e9625771
Roman <sholokh.r@gmail.com>
2021-06-10 01:19:16 +0200

    Rework OkHttp getFromUrl method - now return String

--- src/main/java/com/romansholokh/worldweatheronline/springbootclient/okhttp/OkHttp.java
+++ src/main/java/com/romansholokh/worldweatheronline/springbootclient/okhttp/OkHttp.java
@@ -1,6 +1,5 @@
 package com.romansholokh.worldweatheronline.springbootclient.okhttp;
 
-import net.minidev.json.JSONObject;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -12,16 +11,13 @@ import java.util.Objects;
 @Component
 public class OkHttp {
 
-    public JSONObject getFromUrl(String url) throws IOException {
+    public String getFromUrl(String url) throws IOException {
         OkHttpClient client = new OkHttpClient();
 
         Request request = new Request.Builder().url(url).build();
-
         try (Response response = client.newCall(request).execute()) {
-            JSONObject json = new JSONObject();
-            json.put("response", Objects.requireNonNull(response.body()));
 
-            return json;
+            return Objects.requireNonNull(response.body()).string();
         }
     }
 }
