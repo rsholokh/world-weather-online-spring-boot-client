4fc5f164288bf908ebf7236572c3723d787b038b
Roman <sholokh.r@gmail.com>
2021-06-10 11:33:56 +0200

    Changed schema.sql - now columns may contain more chars

--- src/main/resources/sсhema.sql
+++ src/main/resources/sсhema.sql
@@ -2,10 +2,10 @@ DROP TABLE IF EXISTS weather;
 
 CREATE TABLE weather (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    city VARCHAR(50) NOT NULL,
-    date VARCHAR(10) NOT NULL,
-    max_temp VARCHAR(3) NOT NULL,
-    avg_temp VARCHAR(3) NOT NULL,
-    min_temp VARCHAR(3) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    date VARCHAR(20) NOT NULL,
+    max_temp VARCHAR(10) NOT NULL,
+    avg_temp VARCHAR(10) NOT NULL,
+    min_temp VARCHAR(10) NOT NULL,
     number_of_inquiries INT NOT NULL,
 );
